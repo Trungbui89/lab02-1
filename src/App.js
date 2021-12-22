cdb82182774b62bf627a0294f933083956ea01e9
trungbui89 <2104799703@qq.com>
2021-12-22 09:53:06 +0700

    create lab03-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
 
   render() {
     return (
@@ -11,6 +22,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusions</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
