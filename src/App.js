73296442ba083c9856418198a7ca1fe5c172209b
trungbui89 <2104799703@qq.com>
2022-01-06 17:39:54 +0700

    create lab04-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 import './App.css';
 
 class App extends Component {
@@ -8,12 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusions</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
+        <Main />
       </div>
     );
   }
