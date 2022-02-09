5eeb360bd11fbf446b56e9b562d4a7b1039a639a
trungbui89 <2104799703@qq.com>
2022-02-09 21:16:25 +0700

    end lab5-4

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,50 +1,74 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
-class DishDetail extends Component {
 
-    constructor(props) {
-      super(props);
+function RenderDish({dish}) {
+    return (
+        <div className='col-12 col-md-5 m-1'>
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
+
+function RenderComments({comments}) {
+    if (comments != null) {
+        return (
+            <div className='col-12 col-md-5 m-1'>
+                <h4>Comments</h4>
+                <ul className='list-unstyled'>
+                    {comments.map((comment) => {
+                        return (
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    } else {
+        return (
+            <div></div>
+        )
     }
-    
-    render() {
-        const dishDetail = this.props.dish
-        if (dishDetail != null){
-            const comments = dishDetail.comments.map((comment) => {
-                return (
-                    <div>
-                        <p>{comment.comment}</p>
-                        <p>{comment.author}, {comment.date}</p>
-                    </div>
-                )
-            })
-            
-            return(
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg top src={dishDetail.image} alt={dishDetail.name} />
-                                <CardBody>
-                                    <CardTitle>{dishDetail.name}</CardTitle>
-                                    <CardText>{dishDetail.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <h3>Comment</h3>
-                                {comments}
-                            </Card>
-                        </div>
-                    </div>
+}
+
+const DishDetail = (props) => {
+    if(props.dish != null) {
+        return (
+            <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
                 </div>
-            );
-        }
-        else
-            return(
-                <div></div>
-            );
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+            </div>
+        )
+    } else {
+        return (
+            <div></div>
+        )
     }
 }
 
