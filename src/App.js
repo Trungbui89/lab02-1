bca5214c230073bc9cf63f35bea3af81299f49c4
trungbui89 <2104799703@qq.com>
2022-02-23 13:23:47 +0700

    end lab7-1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
-
 const store = ConfigureStore();
 
 class App extends Component {
