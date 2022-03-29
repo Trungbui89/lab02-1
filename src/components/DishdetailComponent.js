18e7bd36edd77d5c9e59fceefe74019abecbd1f3
trungbui89 <2104799703@qq.com>
2022-03-29 09:31:58 +0700

    end lab10-3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -23,7 +23,7 @@ function RenderDish({dish}) {
     )
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
     if (comments != null) {
         return (
             <div className='col-12 col-md-5 m-1'>
@@ -38,7 +38,7 @@ function RenderComments({comments, addComment, dishId}) {
                         )
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         )
     } else {
@@ -70,7 +70,8 @@ class CommentForm extends Component {
 
     hanldeSubmit(values) {
         this.toggleModal()
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        // this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         console.log(values)
     }
 
@@ -172,7 +173,7 @@ const DishDetail = (props) => {
                 </div>
                 <div className="col-12 col-md-5 m-1">
                 <RenderComments comments={props.comments}
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     dishId={props.dish.id}
                 />
                 </div>
